d725183aaa9dc5c00ff41ef579d6b936dfb4ba26
eruji <eric@therujis.com>
2013-09-11 10:43:02 -0700

    created python 2.7 version of the game

---
+++ hangman_py2.py
@@ -0,0 +1,98 @@
+import random
+import os
+from assets import HANGMANPICS
+
+# absolute path within pythonanywhere
+dictPath = os.path.abspath(os.path.dirname(__file__)) + '/dict.txt'
+
+def getRandomWord():
+    secretWord = random.choice(open(dictPath).readlines())
+    secretWord =  secretWord.strip()
+    return secretWord
+
+def displayBoard(HANGMANPICS, missedLetters, correctLetters, secretWord):
+    os.system('clear') # Clear console in Linux
+    print('E P I C + H A N G M A N')
+    print(HANGMANPICS[len(missedLetters)]) # return the hangman pic array based on number of missed letters
+    print
+
+    print'Missed letters:',
+    for letter in missedLetters:
+        print letter,
+    print
+
+    blanks = '_' * len(secretWord)
+
+    for i in range(len(secretWord)): # replace blanks with correctly guessed letters
+        if secretWord[i] in correctLetters:
+            blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
+
+    for letter in blanks: # show the secret word with spaces in between each letter
+        print letter,
+    print
+
+def getGuess(alreadyGuessed):
+    # Returns the letter the player entered. This function makes sure the player entered a single letter, and not something else.
+    while True:
+        # print('Guess a letter.')
+        guess = raw_input("Guess a Letter.\n")
+        guess = guess.lower()
+        if len(guess) != 1:
+            print('Please enter a single letter.')
+        elif guess in alreadyGuessed:
+            print('You have already guessed that letter. Choose again.')
+        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+            print('Please enter a LETTER.')
+        else:
+            return guess
+
+def playAgain():
+    # This function returns True if the player wants to play again, otherwise it returns False.
+    #print('Do you want to play again? (yes or no)')
+    ans = raw_input("Do you want to play again? (yes or no)")
+    return ans.lower().startswith('y')
+
+
+missedLetters = ''
+correctLetters = ''
+secretWord = getRandomWord()
+gameIsDone = False
+
+while True:
+    displayBoard(HANGMANPICS, missedLetters, correctLetters, secretWord)
+
+    # Let the player type in a letter.
+    guess = getGuess(missedLetters + correctLetters)
+
+    if guess in secretWord:
+        # concat letters to variable
+        correctLetters = correctLetters + guess
+
+        # Check if the player has won
+        foundAllLetters = True
+        for i in range(len(secretWord)):
+            if secretWord[i] not in correctLetters:
+                foundAllLetters = False
+                break
+        if foundAllLetters:
+            print('Yes! The secret word is "' + secretWord + '"! You have won!')
+            gameIsDone = True
+    else:
+        missedLetters = missedLetters + guess
+
+        # Check if player has guessed too many times and lost
+        if len(missedLetters) == len(HANGMANPICS) - 1:
+            displayBoard(HANGMANPICS, missedLetters, correctLetters, secretWord)
+            print('You have run out of guesses!\nAfter ' + str(len(missedLetters)) + ' missed guesses and ' + str(len(correctLetters)) + ' correct guesses, the word was "' + secretWord + '"')
+            gameIsDone = True
+
+    # Ask the player if they want to play again (but only if the game is done).
+    if gameIsDone:
+        if playAgain():
+            # if playAgain() returns TRUE then restart else break
+            missedLetters = ''
+            correctLetters = ''
+            gameIsDone = False
+            secretWord = getRandomWord()
+        else:
+            break
